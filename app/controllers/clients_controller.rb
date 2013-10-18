429a675076971e25e82eed295a8576c4969aca78
jdatacom <jdatacom@san.rr.com>
2013-10-18 01:44:36 -0700

    Add  ability to update a client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -11,6 +11,22 @@ class ClientsController < ApplicationController
     @client = Client.find(params[:id])
   end
 
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    #@client.update(client_params)
+    if @client.update(client_params)
+      flash[:notice] = "Client has been updated."
+      redirect_to @client
+    else
+      flash[:alert] = "Client has not been updated."
+      render "edit"
+    end
+  end
+
   def create
 #    @client = Client.new(params[:client])
     @client = Client.new(client_params)
